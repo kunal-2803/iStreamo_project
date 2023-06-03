9e965e8198aa220632b3e8777a85dcf5300b3867
kunal-2803 <kunal.19bcon179@jecrcu.edu.in>
2023-06-03 10:21:58 +0530

    issue resolved

--- models/user.js
+++ models/user.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    userId: { type: Number, unique: true },
     password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     username: { type: String, required: true, unique: true },
